47b82570ee27a03e32664ae6958f624ab1b56393
Abdul-karim970 <abdulkarim3974@gmail.com>
2023-11-22 18:30:45 +0500

    second

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@
 
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:permission_handler/permission_handler.dart';
+
+const appId = '06b417f44ed0470e9249534d3a603920';
+const appCertificate = '67b6ab170b4b4d7cb78e48f94991f81d';
 
 void main() => runApp(const MyApp());
 
@@ -16,26 +20,27 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CallScreen(),
+      home: const Receiver(),
     );
   }
 }
 
-class CallScreen extends StatefulWidget {
-  const CallScreen({super.key});
+class Receiver extends StatefulWidget {
+  const Receiver({super.key});
 
   @override
-  _CallScreenState createState() => _CallScreenState();
+  _ReceiverState createState() => _ReceiverState();
 }
 
-class _CallScreenState extends State<CallScreen> {
+class _ReceiverState extends State<Receiver> {
   late RtcEngine rtcEngin;
   int? _remoteUid;
   bool _localUserJoined = false;
+  bool isOnLine = false;
   @override
-  void initState() {
-    super.initState();
-    initializeAgora();
+  void didChangeDependencies() async {
+    super.didChangeDependencies();
+    await initializeAgora();
   }
 
   Future<void> initializeAgora() async {
@@ -46,7 +51,7 @@ class _CallScreenState extends State<CallScreen> {
 
     rtcEngin = createAgoraRtcEngine();
     rtcEngin.initialize(const RtcEngineContext(
-        appId: '06b417f44ed0470e9249534d3a603920',
+        appId: appId,
         channelProfile: ChannelProfileType.channelProfileLiveBroadcasting));
 
     rtcEngin.registerEventHandler(
@@ -56,12 +61,16 @@ class _CallScreenState extends State<CallScreen> {
           setState(() {
             _localUserJoined = true;
           });
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('${connection.localUid} joined')));
         },
         onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
           debugPrint("remote user $remoteUid joined");
           setState(() {
             _remoteUid = remoteUid;
           });
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('${connection.localUid} joined')));
         },
         onUserOffline: (RtcConnection connection, int remoteUid,
             UserOfflineReasonType reason) {
@@ -69,72 +78,166 @@ class _CallScreenState extends State<CallScreen> {
           setState(() {
             _remoteUid = null;
           });
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('${connection.localUid} offline')));
         },
         onTokenPrivilegeWillExpire: (RtcConnection connection, String token) {
           debugPrint(
               '[onTokenPrivilegeWillExpire] connection: ${connection.toJson()}, token: $token');
         },
+        onError: (err, msg) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text('$err error')));
+        },
+        onConnectionStateChanged: (connection, state, reason) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text('$state State')));
+          if (state == ConnectionStateType.connectionStateConnected) {
+            isOnLine = true;
+          } else {
+            isOnLine = false;
+          }
+        },
       ),
     );
 
     await rtcEngin.setClientRole(role: ClientRoleType.clientRoleAudience);
+    await rtcEngin.enableAudio();
+    await rtcEngin.enableVideo();
+    await rtcEngin.startPreview();
+    await rtcEngin.enableLocalAudio(true);
   }
 
-  @override
-  void dispose() async {
-    await rtcEngin.leaveChannel();
-    await rtcEngin.release();
-    super.dispose();
-  }
+  // @override
+  // void dispose() async {
+  //   await rtcEngin.leaveChannel();
+  //   await rtcEngin.release();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Agora Audio Call'),
+        title: const Text('Agora Audio Call Receiver'),
       ),
       body: Center(
           child: Column(
         children: [
           ElevatedButton(
               onPressed: () async {
-                try {
-                  await rtcEngin.leaveChannel();
-                  await rtcEngin.joinChannel(
-                    token: 'token',
-                    channelId: 'ak',
-                    uid: 0,
-                    options: const ChannelMediaOptions(
+                // if (isOnLine) {
+
+                // } else {
+                //   ScaffoldMessenger.of(context).showSnackBar(
+                //       SnackBar(content: Text('$_localUserJoined')));
+                // }
+                await rtcEngin.joinChannel(
+                  token: generateToken(
+                      appID: appId,
+                      appCertificate: appCertificate,
+                      uid: 13,
+                      channelName: 'abdul'),
+                  channelId: 'abdul',
+                  uid: 13,
+                  options: const ChannelMediaOptions(
                       autoSubscribeAudio: true,
-                    ),
-                  );
-                } catch (e) {
-                  print('${e.toString()} ====================');
-                }
+                      channelProfile:
+                          ChannelProfileType.channelProfileLiveBroadcasting,
+                      clientRoleType: ClientRoleType.clientRoleAudience),
+                );
               },
-              child: const Text('Join')),
+              child: const Text('Receive')),
           Builder(
             builder: (context) {
               if (_localUserJoined) {
+                SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Joined call')));
+                });
                 return const Text('Joined');
               } else {
+                SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(const SnackBar(content: Text('Waiting')));
+                });
                 return const Text('Waiting');
               }
             },
           ),
-          // Builder(
-          //   builder: (context) {
-          //     if (_remoteUid == null) {
-          //       return const Text('Offline');
-          //     } else {
-          //       return const Text('joined');
-          //     }
-          //   },
-          // ),
+          Builder(
+            builder: (context) {
+              if (_remoteUid == null) {
+                SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('User is offline')));
+                });
+                return const Text('Offline');
+              } else {
+                SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Joined call')));
+                });
+                return const Text('joined');
+              }
+            },
+          ),
+          Stack(
+            children: [
+              Center(
+                child: _remoteVideo(),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: SizedBox(
+                  width: 100,
+                  height: 150,
+                  child: Center(
+                    child: _localUserJoined
+                        ? AgoraVideoView(
+                            controller: VideoViewController(
+                              rtcEngine: rtcEngin,
+                              canvas: const VideoCanvas(uid: 0),
+                            ),
+                          )
+                        : const CircularProgressIndicator(),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       )),
     );
   }
+
+  Widget _remoteVideo() {
+    if (_remoteUid != null) {
+      return AgoraVideoView(
+        controller: VideoViewController.remote(
+          rtcEngine: rtcEngin,
+          canvas: VideoCanvas(uid: _remoteUid),
+          connection: const RtcConnection(channelId: 'abdul'),
+        ),
+      );
+    } else {
+      return const Text(
+        'Please wait for remote user to join',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
 }
 
+String generateToken({
+  required String appID,
+  required String appCertificate,
+  required int uid,
+  required String channelName,
+}) {
+  return '$appID$appCertificate$uid$channelName';
+}
+
+
 // 06b417f44ed0470e9249534d3a603920
+// 67b6ab170b4b4d7cb78e48f94991f81d
